7fcc7087737b955ff196b62466cfc4d02462c7ac
Nagaphaneendra <Nagaphaneendra6240@outlook.com>
2023-03-07 04:30:40 +0000

    Login Authentication API

--- app.js
+++ app.js
@@ -29,19 +29,23 @@ initializeDBAndServer();
 app.post("/register", async (request, response) => {
   const { username, name, password, gender, location } = request.body;
   const hashedPassword = await bcrypt.hash(request.body.password, 10);
+
   const selectUserQuery = `SELECT * FROM user WHERE username = '${username}';`;
   const dbUser = await db.get(selectUserQuery);
   if (dbUser === undefined) {
-    const createUserQuery = `INSERT INTO user (username,name,password,gender,location)
+    let createUserQuery = `INSERT INTO user (username,name,password,gender,location)
         VALUES
         (
             '${username}',
             '${name}','${hashedPassword}','${gender}','${location}');`;
-    await db.run(createUserQuery);
-    response.send("User created successfully");
-  } else if (dbUser.password.length < 5) {
-    response.status(400);
-    response.send("Password is too short");
+    if (password.length < 5) {
+      response.status(400);
+      response.send("Password is too short");
+    } else {
+      let newUserDetails = await db.run(createUserQuery);
+      response.status(200);
+      response.send("User created successfully");
+    }
   } else {
     response.status(400);
     response.send("User already exists");
